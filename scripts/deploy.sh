9bbd37cf972ba36146ec824976cf9e23a86d7498
kirk.kalvar@kal.technology <p8esaq?Ch4br>
2019-11-24 11:22:12 -0500

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,40 +3,40 @@
 if [[ $1 == "create" ]]
   then
 
-    kubectl apply -f kubernetes/
+    kubectl apply -f kube/
 
     aws appmesh create-mesh --mesh-name catdog-eks
     
-    aws appmesh create-virtual-node --cli-input-json file://appmesh/vn-frontend.json
-    aws appmesh create-virtual-node --cli-input-json file://appmesh/vn-cat.json
-    aws appmesh create-virtual-node --cli-input-json file://appmesh/vn-dog.json
+    aws appmesh create-virtual-node --cli-input-json file://mesh/vn-frontend.json
+    aws appmesh create-virtual-node --cli-input-json file://mesh/vn-cat.json
+    aws appmesh create-virtual-node --cli-input-json file://mesh/vn-dog.json
     
-    aws appmesh create-virtual-router --cli-input-json file://appmesh/vr-frontend.json
-    aws appmesh create-virtual-router --cli-input-json file://appmesh/vr-backend.json
+    aws appmesh create-virtual-router --cli-input-json file://mesh/vr-frontend.json
+    aws appmesh create-virtual-router --cli-input-json file://mesh/vr-backend.json
     
-    aws appmesh create-virtual-service --cli-input-json file://appmesh/vs-backend.json
+    aws appmesh create-virtual-service --cli-input-json file://mesh/vs-backend.json
     
-    aws appmesh create-route --cli-input-json file://appmesh/route-frontend.json
-    aws appmesh create-route --cli-input-json file://appmesh/route-backend.json
+    aws appmesh create-route --cli-input-json file://mesh/route-frontend.json
+    aws appmesh create-route --cli-input-json file://mesh/route-backend.json
 
 elif [[ $1 == "update" ]]
   then
 
-    kubectl apply -f kubernetes/
+    kubectl apply -f kube/
 
-    aws appmesh update-virtual-node --cli-input-json file://appmesh/vn-frontend.json
-    aws appmesh update-virtual-node --cli-input-json file://appmesh/vn-cat.json
-    aws appmesh update-virtual-node --cli-input-json file://appmesh/vn-dog.json
-    aws appmesh update-virtual-service --cli-input-json file://appmesh/vs-backend.json
-    aws appmesh update-virtual-router --cli-input-json file://appmesh/vr-frontend.json
-    aws appmesh update-virtual-router --cli-input-json file://appmesh/vr-backend.json
-    aws appmesh update-route --cli-input-json file://appmesh/route-frontend.json
-    aws appmesh update-route --cli-input-json file://appmesh/route-backend.json
+    aws appmesh update-virtual-node --cli-input-json file://mesh/vn-frontend.json
+    aws appmesh update-virtual-node --cli-input-json file://mesh/vn-cat.json
+    aws appmesh update-virtual-node --cli-input-json file://mesh/vn-dog.json
+    aws appmesh update-virtual-service --cli-input-json file://mesh/vs-backend.json
+    aws appmesh update-virtual-router --cli-input-json file://mesh/vr-frontend.json
+    aws appmesh update-virtual-router --cli-input-json file://mesh/vr-backend.json
+    aws appmesh update-route --cli-input-json file://mesh/route-frontend.json
+    aws appmesh update-route --cli-input-json file://mesh/route-backend.json
 
 elif [[ $1 == "delete" ]]
   then
 
-    kubectl delete -f kubernetes/
+    kubectl delete -f kube/
 
     aws appmesh delete-virtual-node --mesh-name catdog-eks --virtual-node-name catdog-frontend
     aws appmesh delete-virtual-node --mesh-name catdog-eks --virtual-node-name catdog-cat
@@ -51,8 +51,8 @@ elif [[ $1 == "delete" ]]
 elif [[ $1 == "update-routes" ]]
   then
 
-    aws appmesh update-route --cli-input-json file://appmesh/route-frontend.json
-    aws appmesh update-route --cli-input-json file://appmesh/route-backend.json
+    aws appmesh update-route --cli-input-json file://mesh/route-frontend.json
+    aws appmesh update-route --cli-input-json file://mesh/route-backend.json
 
 else
   echo "Give me an argument -- create, update, or delete"
